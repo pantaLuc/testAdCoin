cbafa36ef0bffc3ad232fd33a2b5918044130eb1
Luc Perin Panta <lucpanta@MacBook-Pro-de-Luc.local>
2022-04-26 05:55:47 +0200

    adding controller

--- src/dogs/dogs.controller.ts
+++ src/dogs/dogs.controller.ts
@@ -1,13 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { Dog } from './dogs.entity';
 import { DogsService } from './dogs.service';
 
 @Controller('dogs')
 @ApiTags('dogs')
 export class DogsController {
   constructor(private dogService: DogsService) {}
+  // route for all dogs
   @Get()
   findAll() {
     return this.dogService.showAll();
+  }
+  // find a Dog
+  @Post()
+  async createUsers(@Body() data: Dog) {
+    const dog = await this.dogService.create(data);
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'Dog created successfully',
+      dog,
+    };
   }
 }
--- src/dogs/dogs.service.ts
+++ src/dogs/dogs.service.ts
@@ -12,10 +12,22 @@ export class DogsService {
     return await this.repo.find();
   }
   // create a dog
-
   async create(data: Dog) {
     const user = this.repo.create(data);
     await this.repo.save(data);
     return user;
+  }
+  // find by id
+  async read(id: number) {
+    return await this.repo.findOne({ where: { id: id } });
+  }
+  //. update
+  async update(id: number, data: Partial<Dog>) {
+    await this.repo.update({ id }, data);
+    return await this.repo.findOne({ id });
+  }
+  async destroy(id: number) {
+    await this.repo.delete({ id });
+    return { deleted: true };
   }
 }
